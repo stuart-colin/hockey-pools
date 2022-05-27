5d693998dd61f289ffdf8c3fec2f7cdb889041cd
Colin Stuart <stuart.colin@gmail.com>
2022-05-27 01:32:55 -0400

    Fixed user endpoint

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -3,7 +3,8 @@ import countPoints from '../utils/countPoints';
 import eliminatedPlayers from '../utils/eliminatedPlayers';
 
 // const userEndpoint = 'https://nhl-pools-api-efhcx3qyra-uc.a.run.app/v1/users?limit=100';
-const rosterEndpoint = 'http://localhost:5000/v1/rosters';
+const rosterEndpoint = 'https://nhl-pools-api-efhcx3qyra-uc.a.run.app/v1/rosters';
+// const rosterEndpoint = 'http://localhost:5000/v1/rosters';
 
 const useUsers = () => {
   const [userList, setUserList] = useState([]);
