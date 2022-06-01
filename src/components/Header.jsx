b0667665980042ba718e6c5c791a0da9d44280a6
Colin Stuart <stuart.colin@gmail.com>
2022-06-01 01:52:35 -0400

    Tweaked header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import useScores from '../hooks/useScores';
 const Header = () => {
   return (
     <h1 className='ui center aligned icon header'>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/6/6a/Ice_hockey_puck.svg' style={{ width: 100 }} alt='hockey puck icon' />
       <div className='content'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/6/6a/Ice_hockey_puck.svg' style={{ width: 60, position: 'relative', top: '0.5em', left: '-0.5em' }} alt='hockey puck icon' />
         <em className='ui blue header'>BP's Annual Hockey Pool</em>
       </div>
       <div className='ui horizontal divider'>
