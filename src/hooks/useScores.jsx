0fb99875ed6ab930cdd5e97108b4030c08771fd6
Colin Stuart <stuart.colin@gmail.com>
2024-04-19 15:54:04 -0400

    Testing cors proxy again

--- src/hooks/useScores.jsx
+++ src/hooks/useScores.jsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from 'react';
 
 // const date = new Date().toLocaleDateString().split('/');
 // const scoreDetailsEndpoint = 'https://nhl-score-api.herokuapp.com/api/scores?startDate=' + date[2] + '-' + date[0] + '-' + date[1] - 1;
-const scoreDetailsEndpointNHL = 'https://cors-anywhere.herokuapp.com/https://api-web.nhle.com/v1/score/now'
-// const scoreDetailsEndpointNHL = 'https://api-web.nhle.com/v1/score/now'
+// const scoreDetailsEndpointNHL = 'https://cors-anywhere.herokuapp.com/https://api-web.nhle.com/v1/score/now'
+// const scoreDetailsEndpointNHL = 'http://localhost:8080/https://api-web.nhle.com/v1/score/now'
+const scoreDetailsEndpointNHL = 'https://api-web.nhle.com/v1/score/now'
+// const scoreDetailsEndpointNHL = 'https://api-web.nhle.comcd/v1/score/now'
+
 const useScores = () => {
   const [date, setDate] = useState('');
   const [games, setGames] = useState([]);
 
   useEffect(() => {
     const getScoreData = async () => {
-      const res = await fetch(scoreDetailsEndpointNHL);
+      const res = await fetch(`/cors-proxy/${scoreDetailsEndpointNHL}`);
       const json = await res.json();
       if (json.currentDate) {
         setDate(json.currentDate);
