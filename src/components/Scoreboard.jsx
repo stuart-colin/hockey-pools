c0c8c0d34c9d1893c0328458472fbee0cf7e0d70
Colin Stuart <stuart.colin@gmail.com>
2025-04-28 04:26:46 -0400

    Added scorer and assist information to Scoreboard.

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { List, Label, Image, Segment, Header, Icon } from 'semantic-ui-react';
+import { List, Label, Image, Segment, Popup, Icon } from 'semantic-ui-react';
 import useScores from '../hooks/useScores';
 import getOrdinals from '../utils/getOrdinals';
+
+const teamLogo = 'https://assets.nhle.com/logos/nhl/svg/';
 
 const Scoreboard = () => {
   const scoreboard = useScores();
@@ -21,27 +23,73 @@ const Scoreboard = () => {
     });
   }
 
+  const renderGoalDetails = (goals) => {
+    return (
+      <List divided relaxed>
+        {goals.map((goal, index) => (
+          <List.Item
+            key={index}
+            style={{
+              display: "flex", // Use flexbox for horizontal alignment
+              alignItems: "center", // Vertically center the content
+            }}
+          >
+            <Image
+              avatar
+              src={teamLogo + goal.teamAbbrev + '_light.svg'} // Assuming the team logo URL is stored here
+              alt={`${goal.teamName} Logo`}
+              style={{ marginRight: "10px" }}
+            />
+            <Image
+              avatar
+              src={goal.mugshot} // Assuming the mugshot URL is stored here
+              alt={`${goal.name.default}'s mugshot`}
+              style={{ marginRight: "10px" }}
+            />
+            <List.Content>
+              <List.Header>
+                <strong>G:</strong> {goal.name.default}
+              </List.Header>
+              <List.Description>
+                <strong>A:</strong>{" "}
+                {goal.assists.length > 0
+                  ? goal.assists.map((assist) => assist.name.default).join(", ")
+                  : "None"}
+              </List.Description>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    );
+  };
+
   const games = scoreboard.games.map((game, index) => (
     <List.Item key={index}>
-      <Label>
-        <Image avatar src={game.awayTeam.logo} alt={`${game.awayTeam.name.default} Logo`} />
-        <Label>
-          {game.awayTeam.score}
-        </Label>
-        <Image avatar src={game.homeTeam.logo} alt={`${game.homeTeam.name.default} Logo`} />
-        <Label>
-          {game.homeTeam.score}
-        </Label>
-        <Label>
-          {game.gameState === 'OFF' || game.gameState === 'FINAL'
-            ? 'Final'
-            : game.gameState === 'FUT' || game.gameState === 'PRE'
-              ? localDate(game.startTimeUTC)
-              : `${game.clock.timeRemaining}
+      <Popup
+        trigger={
+          <Label>
+            <Image avatar src={game.awayTeam.logo} alt={`${game.awayTeam.name.default} Logo`} />
+            <Label>
+              {game.awayTeam.score}
+            </Label>
+            <Image avatar src={game.homeTeam.logo} alt={`${game.homeTeam.name.default} Logo`} />
+            <Label>
+              {game.homeTeam.score}
+            </Label>
+            <Label>
+              {game.gameState === 'OFF' || game.gameState === 'FINAL'
+                ? 'Final'
+                : game.gameState === 'FUT' || game.gameState === 'PRE'
+                  ? localDate(game.startTimeUTC)
+                  : `${game.clock.timeRemaining}
           ${game.period}${getOrdinals(game.period)}
           ${game.clock.inIntermission ? 'INT' : ''}`}
-        </Label>
-      </Label>
+            </Label>
+          </Label>
+        } content={renderGoalDetails(game.goals)}
+        position="top center"
+        hoverable
+      />
     </List.Item>
   ));
 
